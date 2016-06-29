1e529d46336ad81fbf6ff8ad479ab435d30c6310
Christine L. Feaster <feasterc@gmail.com>
2016-06-28 21:11:30 -0700

    Add Products controller and routes

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,40 @@
+class ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
+  respond_to :html, :json
+
+  def index
+    @products = Product.all
+  end
+
+  def show
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def edit
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @product.user = current_user
+    @product.save
+    respond_with(@product)
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+    def set_product
+      @product = Product.find(params[:id])
+    end
+
+    def product_params
+      params.require(:product).permit(:name, :permalink, :description, :price)
+    end
+end
