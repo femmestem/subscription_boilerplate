567bbb47d5aa2c8d12d21bf5aea43fec0cbfd106
Christine L. Feaster <feasterc@gmail.com>
2016-06-29 16:27:43 -0700

    Add Product#update and Product#destroy

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -24,9 +24,23 @@ class ProductsController < ApplicationController
   end
 
   def update
+    respond_to do |format|
+      if @product.update(product_params)
+        format.html { redirect_to @product, notice: 'Product was successfully updated.' }
+        format.json { render :show, status: :ok, location: @product }
+      else
+        format.html { render :edit }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
+    @product.destroy
+    respond_to do |format|
+      format.html { redirect_to products_url, notice: 'Product was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
 
   private
