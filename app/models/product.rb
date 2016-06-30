a6253d7520cc213daf34dd670fdb634aceecc494
Christine L. Feaster <feasterc@gmail.com>
2016-06-29 19:15:18 -0700

    Add validations to Product
     - Add validation for type of content that can be uploaded as a product
     - Add validations for price as a number greater than 0

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,18 @@ class Product < ActiveRecord::Base
   belongs_to :user
   has_many :sales
 
+  validates_numericality_of :price,
+    greater_than: 0,
+    message: "price must be greater than 0"
+
   has_attached_file :file
+
+  validates_attachment_content_type :file, :content_type => [
+    "image/jpg",
+    "image/jpeg",
+    "image/png",
+    "image/gif",
+    "application/pdf",
+    "application/zip"
+  ]
 end
