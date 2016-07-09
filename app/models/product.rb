49bf2406b01916ea9b95269cece1fe880864b547
Christine L. Feaster <feasterc@gmail.com>
2016-07-08 22:24:20 -0700

    Add validations for Product file attachments
     - Since Products are downloable digital files, validate presence of file attachment
     - Restrict Product file size to 2 MB

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,19 +1,23 @@
 class Product < ActiveRecord::Base
   belongs_to :user
   has_many :sales
+  has_attached_file :file
 
   validates_numericality_of :price,
     greater_than: 50,
     message: "price must be greater than 50 cents"
 
-  has_attached_file :file
-
-  validates_attachment_content_type :file, :content_type => [
-    "image/jpg",
-    "image/jpeg",
-    "image/png",
-    "image/gif",
-    "application/pdf",
-    "application/zip"
-  ]
+  validates_attachment :file,
+    presence: true,
+    size: { less_than: 2.megabytes }
+    content_type: {
+      content_type: [
+        "image/jpg",
+        "image/jpeg",
+        "image/png",
+        "image/gif",
+        "application/pdf",
+        "application/zip"
+      ]
+    },
 end
